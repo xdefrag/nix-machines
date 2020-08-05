a2db3989a9b1b1e66004d661a517c62bca6d4890
Stanislav Karkavin <me@xdefrag.dev>
2020-08-05 10:54:02 +0300

    Add comfort zone start config

---
+++ comfort-zone.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+with builtins;
+
+let
+  vimrc = fetchurl
+    "https://raw.githubusercontent.com/xdefrag/nix-config/master/dotfiles/vim/vimrc";
+  vimc =
+    pkgs.vim_configurable.customize { 
+      name = "vim";
+      vimrcConfig.customRC = readFile vimrc;
+    };
+  bashrc = fetchurl
+    "https://raw.githubusercontent.com/mrzool/bash-sensible/master/sensible.bash";
+in {
+  environment.etc."bashrc.local".text = (readFile bashrc + ''
+    alias v='vim'
+  '');
+  environment.systemPackages = with pkgs; [ git vimc ];
+
+  networking.firewall.enable = true;
+
+  users.users.xdefrag = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    uid = 1000;
+    shell = pkgs.bash;
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 7d";
+  };
+}
